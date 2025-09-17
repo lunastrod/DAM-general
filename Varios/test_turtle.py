780d867e0d538a8178a42cd00ee4e2e3af257cc0
lunastrod <daniel.parra.segovia@gmail.com>
2025-09-17 14:20:23 +0200

    base de datos ejemlo

--- Varios/test_turtle.py
+++ Varios/test_turtle.py
@@ -2,33 +2,28 @@
 import turtle
 import math
 import time
-"""
-turtle.speed(0)
-angle=-200
-step=-1
-zoom=5
-while True:
-    turtle.forward(angle*zoom)
-    turtle.left(angle)
-    angle+=step
-    if angle > 200 or angle < -200:
-        step=-step
-    turtle.color((abs(angle/250),0.1,0.1))
-"""
+import random
 class rocket:
     def __init__(self):
+        self.t = turtle.Turtle()
+        self.t.goto((0,0))
+        self.t.speed(0)
+        self.t.penup()
+        self.t.shapesize(2)
         self.p=[0,0]
         self.v=[0,0]
         self.a=[0,0]
         self.w=0
         self.angle=0
         self.dt=1
+        self.screen_size=[500,500]
     def step(self):
         self.angle=self.angle+self.w
         self.v=[self.v[0]+self.a[0]*self.dt,self.v[1]+self.a[1]*self.dt]
         self.p=[self.p[0]+self.v[0]*self.dt,self.p[1]+self.v[1]*self.dt]
-        turtle.goto(self.p)
-        turtle.setheading(self.angle/(math.pi)*180)
+        self.checkborders()
+        self.t.goto(self.p)
+        self.t.setheading(self.angle/(math.pi)*180)
         print(self.p,self.v,self.a)
         self.a=[0,0]
     def fd(self):
@@ -44,26 +39,57 @@ class rocket:
     def rt(self):
         self.w+=0.05
         self.step()
+    def checkborders(self):
+        if(self.p[0]<-self.screen_size[0]):
+            self.p[0]=self.screen_size[0]-1
+        if(self.p[0]>self.screen_size[0]):
+            self.p[0]=-self.screen_size[0]+1
+        if(self.p[1]<-self.screen_size[1]):
+            self.p[1]=self.screen_size[1]-1
+        if(self.p[1]>self.screen_size[1]):
+            self.p[1]=-self.screen_size[1]+1
 
-    
+class Asteroid():
+    def __init__(self):
+        self.t = turtle.Turtle()
+        self.t.goto((0,0))
+        self.t.speed(0)
+        self.t.penup()
+        self.t.shapesize(3)
+        self.screen_size=[500,500]
+        self.p=[random.randint(-self.screen_size[0],self.screen_size[0]),random.randint(-self.screen_size[1],self.screen_size[1])]
+        self.v=[random.randint(-500,500)/100,random.randint(-500,500)/100]
+        self.a=[0,0]
+        self.dt=1
+        self.t.shape("circle")
+    def step(self):
+        self.v=[self.v[0]+self.a[0]*self.dt,self.v[1]+self.a[1]*self.dt]
+        self.p=[self.p[0]+self.v[0]*self.dt,self.p[1]+self.v[1]*self.dt]
+        self.checkborders()
+        self.t.goto(self.p)
+        self.a=[0,0]
+    def checkborders(self):
+        if(self.p[0]<-self.screen_size[0]):
+            self.p[0]=self.screen_size[0]-1
+        if(self.p[0]>self.screen_size[0]):
+            self.p[0]=-self.screen_size[0]+1
+        if(self.p[1]<-self.screen_size[1]):
+            self.p[1]=self.screen_size[1]-1
+        if(self.p[1]>self.screen_size[1]):
+            self.p[1]=-self.screen_size[1]+1
 
-"""
-def fd():
-    turtle.forward(10)
-def lt():
-    turtle.left(5)
-def rt():
-    turtle.right(5)
-turtle.onkeypress(fd,"w")
-turtle.onkeypress(lt,"a")
-turtle.onkeypress(rt,"d")
-"""
 r=rocket()
-turtle.onkey(r.fd,"w")
-turtle.onkey(r.lt,"a")
-turtle.onkey(r.rt,"d")
-turtle.goto((0,0))
+asteroids=[]
+for i in range(10):
+    asteroids.append(Asteroid())
+screen = turtle.Screen()
+screen.setup(1020,1020)
+screen.onkey(r.fd,"w")
+screen.onkey(r.rt,"a")
+screen.onkey(r.lt,"d")
 while True:
     r.step()
-    turtle.listen()
-    time.sleep(0.03)
+    for a in asteroids:
+        a.step()
+    screen.listen()
+    #time.sleep(0.01)
