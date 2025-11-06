d3de25ae8967ae274b18d9e50c8e5c0a3e516381
lunastrod <daniel.parra.segovia@gmail.com>
2025-11-06 14:10:35 +0100

    6-nov

--- Varios/Varios/Ejemplo.java
+++ Varios/Varios/Ejemplo.java
@@ -2,8 +2,15 @@ import java.util.Scanner;
 
 public class Ejemplo {
     public static void main(String[] args){
-        int entero;//declaración
-        entero=1;//inicialización
-        int otroEntero=2;//declaración e inicialización
+        double n=1765765.7654;
+        String nStr=Double.toString(n);
+        int indexPunto=nStr.indexOf(".");
+        String parteEntera=nStr.substring(0, indexPunto);
+        System.out.println();
+        System.out.println();
+        System.out.println(parteEntera);
+        int entero=Integer.parseInt(parteEntera);
+        System.out.println(parteEntera);
+
     }
-}
+} 
