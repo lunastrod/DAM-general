4189278bb8f33215fbaef56dd39a5928fde42397
lunastrod <daniel.parra.segovia@gmail.com>
2026-01-14 21:09:15 +0100

    miau

--- Programacion/Actividades/src/com/dam/jan8/Producto.java
+++ Programacion/Actividades/src/com/dam/jan8/Producto.java
@@ -1,8 +1,8 @@
 package com.dam.jan8;
 
 public class Producto {
-    String nombre;
-    int cantidad;
+    private String nombre;
+    private int cantidad;
 
     public Producto(String nombre, int cantidad) {
         this.nombre = nombre;
@@ -12,4 +12,34 @@ public class Producto {
     public String toString() {
         return cantidad+" "+nombre+", ";
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+        result = prime * result + cantidad;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Producto other = (Producto) obj;
+        if (nombre == null) {
+            if (other.nombre != null)
+                return false;
+        } else if (!nombre.equals(other.nombre))
+            return false;
+        if (cantidad != other.cantidad)
+            return false;
+        return true;
+    }
+
+    
 }
