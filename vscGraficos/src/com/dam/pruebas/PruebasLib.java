28050443b9c98b1e6389b6a040302850fc53b4d5
lunastrod <daniel.parra.segovia@gmail.com>
2026-02-20 12:43:18 +0100

    a

--- vscGraficos/src/com/dam/pruebas/PruebasLib.java
+++ vscGraficos/src/com/dam/pruebas/PruebasLib.java
@@ -1,24 +1,23 @@
 package com.dam.pruebas;
-
-import java.awt.Color;
-import java.awt.Label;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.WindowConstants;
 
-import com.dam.lib.libGraficos1;
+import com.dam.lib.Graf1;
 
 public class PruebasLib {
 
 	public static void main(String[] args) throws InterruptedException {
-		JFrame marco = libGraficos1.confFrame(new JFrame(), "Ventana Hola Mundo", 1000, 1000, null);
-        JLabel lblSaludo=(JLabel) libGraficos1.confComp(new JLabel(), marco,"Hola!", 150, 150, 200, 50);
-        JLabel lblSaludo2=(JLabel) libGraficos1.confComp(new JLabel(), marco,"Hola!", 200, 200, 200, 50);
-        JButton lblSaludo3=(JButton) libGraficos1.confComp(new JButton(), marco,"Hola!", 250, 250, 200, 50);
+		JFrame marco = Graf1.newF(new JFrame(), "Ventana Hola Mundo", 1000, 1000);
+        JLabel lblSaludo=(JLabel) Graf1.newC(new JLabel(), marco,"Hola!");
+        JLabel lblSaludo2=(JLabel) Graf1.newC(new JLabel(), marco,"Hola!");
+        JButton lblSaludo3=(JButton) Graf1.newC(new JButton(), marco,"H");
+        JButton autosize=(JButton) Graf1.newC(new JButton(), marco,"aaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        Graf1.moveBelow(lblSaludo3, autosize, 0);
+        Graf1.moveBelow(lblSaludo, lblSaludo3, 0);
+        Graf1.moveBelow(lblSaludo2, lblSaludo, 0);
 
-        marco.setVisible(true);
+        Graf1.show(marco);
 	}
 
 }
