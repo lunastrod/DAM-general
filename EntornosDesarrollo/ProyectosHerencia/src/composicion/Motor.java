a27f93b61db8955467c247080361dfec6abaa163
lunastrod <daniel.parra.segovia@gmail.com>
2026-04-08 10:39:42 +0200

    miau

---
+++ EntornosDesarrollo/ProyectosHerencia/src/composicion/Motor.java
@@ -0,0 +1,22 @@
+package composicion;
+
+public class Motor {
+    private int numMotor;
+
+    public Motor(int numMotor) {
+        this.numMotor = numMotor;
+    }
+
+    public Motor() {
+    }
+
+    public int getNumMotor() {
+        return numMotor;
+    }
+
+    public void setNumMotor(int numMotor) {
+        this.numMotor = numMotor;
+    }
+
+
+}
